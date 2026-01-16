f025395ca0812c2eb42c7b5bdac47a6024b8f76e
Pavel Klyuev <p.klyuev@jvo.ru>
2026-01-16 12:38:11 +0300

    ag grid support

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { ClientSideRowModelModule, ModuleRegistry } from 'ag-grid-community'
+import { LicenseManager, TreeDataModule } from 'ag-grid-enterprise'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+ModuleRegistry.registerModules([ClientSideRowModelModule, TreeDataModule])
+LicenseManager.setLicenseKey(import.meta.env.VITE_AG_GRID_KEY)
 
 app.mount('#app')
