1d0819619fa62f3b2cf9b9022592b5fcf3bfa1d5
Pavel Klyuev <p.klyuev@jvo.ru>
2026-01-16 14:06:50 +0300

    added ag grid row numbers module

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,20 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { ClientSideRowModelModule, ModuleRegistry } from 'ag-grid-community'
-import { LicenseManager, TreeDataModule } from 'ag-grid-enterprise'
+import { ClientSideRowModelModule, ModuleRegistry, ValidationModule } from 'ag-grid-community'
+import { LicenseManager, RowNumbersModule, TreeDataModule } from 'ag-grid-enterprise'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-ModuleRegistry.registerModules([ClientSideRowModelModule, TreeDataModule])
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  RowNumbersModule,
+  TreeDataModule,
+  ValidationModule,
+])
 LicenseManager.setLicenseKey(import.meta.env.VITE_AG_GRID_KEY)
 
 app.mount('#app')
